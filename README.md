a48aaf563f02d7ed4c63e2445d9e71b46ad1bf3c
Carl Steffen <carl@stonefin.com>
2017-08-25 05:33:48 -0500

    Start with simple PHP file and Readme - bare minimum content Contains simple IF block to prevent direct access to the file - i.e. if someone tried to put an address into their browser like http://mysite.com/content/plugins/myplugin.php - it should not do anything.

---
+++ README.md
@@ -0,0 +1,2 @@
+# multi-store-google-places-shortcodes
+Wordpress plugin for adding shortcodes that a)display business hours from the Google Places api and b) a Google Map with pins on each store location
---
+++ multi-store-google-places-shortcodes.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Plugin Name: Multi Store Google Places ShortCodes Settings
+Plugin URI: https://github.com/StoneFin/multi-store-google-places-shortcodes
+Description: Right now it does nothing.
+Version: 0.1 BETA
+Author: Stone Fin Technology
+Author URI: https://www.stonefin.com
+*/
+/** This is a basic wordpress plugin...
+ * the comment section above tells WordPress that
+ * this file should be loaded when wordpress runs.
+ * All future files created for this plugin should
+ * run through this main file
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+?>
